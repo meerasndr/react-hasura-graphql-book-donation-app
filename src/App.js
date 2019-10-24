347fd3a64ff248c95045d0851e2f806227f91962
Meera Sundar <mirage.sundial@gmail.com>
2019-10-24 20:53:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,27 +7,35 @@ class App extends React.Component {
     //below function passes book data(title, author) to the backend via GraphQL
     addBook = (title,author) => {
 
-    axios({                                                        //HTTP request sent via Axios 
+    axios({                                                        //HTTP request sent via Axios
         url: 'https://book-donation-app.herokuapp.com/v1/graphql', //GraphQL Endpoint created using Hasura, deployed on Heroku
         method: 'post',
         data: { query:
-        `mutation{
+        `mutation upsert_donatedbooks {
             insert_donatedbooks(objects:[
-            {
+              {
                 book_title: "${title}",
                 myauthor:{
-                data: {
+                  data: {
                     name: "${author}"
+                  },
+            on_conflict: {
+                constraint: authors_name_key,
+                update_columns: [name]
+            }     
                 }
-                }
-            }
-        ]){ affected_rows}
-      }`
+              }
+            ]
+            )
+            {affected_rows}
+          }`
     }
       }).then((result) => {
         console.log(result.data)
       });
 }
+
+
 
 //Render and pass props
 render(){
